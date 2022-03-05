ea9754ca4ac56918d0b22828ce25fe53187ddea2
Or Kopel <smilgarden@gmail.com>
2022-03-05 23:18:32 +0200

    more changes4

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -9,25 +9,25 @@ export default {
             <nav class="nav-bar" v-if="seen" >
                 <router-link to="/">
                     <span class="img-menu-span hover">
-                        <img class="menu-img" src="/img/home.png" alt="">
+                        <img class="menu-img" src="./img/home.png" alt="">
                     </span>
                 </router-link> 
 
                 <router-link to="/appsus/note">
                     <span class="img-menu-span hover" >
-                        <img class="menu-img" src="/img/notes.png" alt="">
+                        <img class="menu-img" src="./img/notes.png" alt="">
                     </span>
                 </router-link> 
 
                 <router-link to="/appsus/mail">
                     <span class="img-menu-span hover">
-                        <img class="menu-img" src="/img/email.png" alt="">
+                        <img class="menu-img" src="./img/email.png" alt="">
                     </span>
                 </router-link> 
 
                 <router-link to="/appsus/book">
                     <span class="img-menu-span hover">
-                        <img class="menu-img" src="/img/open-book.png" alt="">
+                        <img class="menu-img" src="./img/open-book.png" alt="">
                     </span>
                 </router-link> 
                 <!-- <router-link to="/book">Book Shop</router-link> | -->
